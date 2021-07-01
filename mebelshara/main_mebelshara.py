870473ac2fa69dd776943599768e907b84896464
Evgeniy Asipkov <easipkov@gmail.com>
2021-07-01 20:33:48 +0300

    mini update

--- mebelshara/main_mebelshara.py
+++ mebelshara/main_mebelshara.py
@@ -2,43 +2,54 @@ from bs4 import BeautifulSoup
 import json
 import requests
 
-url = 'https://www.mebelshara.ru/contacts'
 
-headers = {
-    "Accept": "*/*",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/91.0.4472.77 Safari/537.36"
-}
-req = requests.get(url, headers=headers)
-scr = req.text
+def save_html():
+    url = 'https://www.mebelshara.ru/contacts'
 
-with open('mebelshara/mebelshara.html', 'w') as f:
-    f.write(scr)
+    headers = {
+        "Accept": "*/*",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/91.0.4472.77 Safari/537.36"
+    }
+    req = requests.get(url, headers=headers)
+    scr = req.text
 
-with open('mebelshara/mebelshara.html') as file:
-    all_html = file.read()
+    with open('mebelshara/mebelshara.html', 'w') as f:
+        f.write(scr)
+    return print('HTML-код страницы сайта сохранен')
 
-soup = BeautifulSoup(all_html, 'lxml')
 
-all_city = soup.find_all(class_='city-item')
+def parsing_html():
+    with open('mebelshara/mebelshara.html') as file:
+        all_html = file.read()
 
-project_data_list = []
+    soup = BeautifulSoup(all_html, 'lxml')
 
-for one_city in all_city:
-    while one_city.find(class_='shop-list-item'):
-        info_item = one_city.find(class_='shop-list-item')
-        working_hours_1 = info_item.get("data-shop-mode1")
-        working_hours_2 = info_item.get("data-shop-mode2")
-        project_data_list.append({
-            'address': f'{one_city.find(class_="js-city-name").text}, {info_item.get("data-shop-address")}',
-            'latlon': f'[{info_item.get("data-shop-latitude")}, {info_item.get("data-shop-longitude")}]',
-            'name': "Мебель Шара",
-            'phones': f'[{info_item.get("data-shop-phone")}]',
-            'working_hours': f"[пн - вс {working_hours_2}]" if working_hours_1 == "Без выходных:" else
-            f"[{working_hours_1}, {working_hours_2}]",
-        }
-        )
-        info_item.decompose()
+    all_city = soup.find_all(class_='city-item')
 
-with open('mebelshara/mebelshara_result.json', 'a', encoding='utf-8') as json_file:
-    json.dump(project_data_list, json_file, indent=4, ensure_ascii=False)
+    project_data_list = []
+
+    for one_city in all_city:
+        while one_city.find(class_='shop-list-item'):
+            info_item = one_city.find(class_='shop-list-item')
+            working_hours_1 = info_item.get("data-shop-mode1")
+            working_hours_2 = info_item.get("data-shop-mode2")
+            project_data_list.append({
+                'address': f'{one_city.find(class_="js-city-name").text}, {info_item.get("data-shop-address")}',
+                'latlon': f'[{info_item.get("data-shop-latitude")}, {info_item.get("data-shop-longitude")}]',
+                'name': "Мебель Шара",
+                'phones': f'[{info_item.get("data-shop-phone")}]',
+                'working_hours': f"[пн - вс {working_hours_2}]" if working_hours_1 == "Без выходных:" else
+                f"[{working_hours_1}, {working_hours_2}]",
+            }
+            )
+            info_item.decompose()
+
+    with open('mebelshara/mebelshara_result.json', 'a', encoding='utf-8') as json_file:
+        json.dump(project_data_list, json_file, indent=4, ensure_ascii=False)
+    return print('Данные по городам сохранены в json-файл')
+
+
+if __name__ == '__main__':
+    save_html()
+    parsing_html()
--- tui/main_tui.py
+++ tui/main_tui.py
@@ -1,55 +1,63 @@
 import json
 import requests
 
-url = 'https://apigate.tui.ru/api/office/cities'
 
-headers = {
-    "Accept": "*/*",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/91.0.4472.77 Safari/537.36"
-}
-req = requests.get(url, headers=headers)
-scr = req.text
+def parsing_api():
+    url = 'https://apigate.tui.ru/api/office/cities'
 
-all_cities_id = []
-project_data_list = []
+    headers = {
+        "Accept": "*/*",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/91.0.4472.77 Safari/537.36"
+    }
+    req = requests.get(url, headers=headers)
+    scr = req.text
 
-dict_scr = json.loads(scr)
-values_cities = dict_scr['cities']
+    all_cities_id = []
+    project_data_list = []
 
-for id_ in values_cities:
-    all_cities_id.append(id_['cityId'])
+    dict_scr = json.loads(scr)
+    values_cities = dict_scr['cities']
 
-for el in all_cities_id:
-    url_city = f'https://apigate.tui.ru/api/office/list?cityId={el}&subwayId=&hoursFrom=&hoursTo=&serviceIds=all&toBeOp' \
-               f'enOnHolidays=false'
-    req_city = requests.get(url_city, headers=headers)
-    scr_city = req_city.text
-    dict_scr_city = json.loads(scr_city)
-    values_city = dict_scr_city['offices']
-    for one_office in values_city:
-        if one_office:
-            working_hours = one_office['hoursOfOperation']
-            if working_hours['saturday']['isDayOff'] is False and working_hours['sunday']['isDayOff'] is False:
-                working_hours_weekend = f'сб {working_hours["saturday"]["startStr"]} до {working_hours["saturday"]["endStr"]}, ' \
-                                        f'вс {working_hours["sunday"]["startStr"]} до {working_hours["saturday"]["endStr"]}'
-                if working_hours["saturday"]["startStr"] and working_hours["saturday"]["endStr"] == working_hours["sunday"]["startStr"]\
-                        and working_hours["saturday"]["endStr"]:
+    for id_ in values_cities:
+        all_cities_id.append(id_['cityId'])
+
+    for el in all_cities_id:
+        url_city = f'https://apigate.tui.ru/api/office/list?cityId={el}&subwayId=&hoursFrom=&hoursTo=&serviceIds=all&toBeOp' \
+                   f'enOnHolidays=false'
+        req_city = requests.get(url_city, headers=headers)
+        scr_city = req_city.text
+        dict_scr_city = json.loads(scr_city)
+        values_city = dict_scr_city['offices']
+        for one_office in values_city:
+            if one_office:
+                working_hours = one_office['hoursOfOperation']
+                if working_hours['saturday']['isDayOff'] is False and working_hours['sunday']['isDayOff'] is False:
+                    working_hours_weekend = f'сб {working_hours["saturday"]["startStr"]} до {working_hours["saturday"]["endStr"]}, ' \
+                                            f'вс {working_hours["sunday"]["startStr"]} до {working_hours["saturday"]["endStr"]}'
+                    if working_hours["saturday"]["startStr"] and working_hours["saturday"]["endStr"] == \
+                            working_hours["sunday"]["startStr"] \
+                            and working_hours["saturday"]["endStr"]:
+                        working_hours_weekend = f'сб-вс {working_hours["saturday"]["startStr"]} до {working_hours["saturday"]["endStr"]}'
+                elif working_hours['saturday']['isDayOff'] is False and working_hours['sunday']['isDayOff'] is True:
                     working_hours_weekend = f'сб-вс {working_hours["saturday"]["startStr"]} до {working_hours["saturday"]["endStr"]}'
-            elif working_hours['saturday']['isDayOff'] is False and working_hours['sunday']['isDayOff'] is True:
-                working_hours_weekend = f'сб-вс {working_hours["saturday"]["startStr"]} до {working_hours["saturday"]["endStr"]}'
-            else:
-                working_hours_weekend = "сб - вс выходной"
+                else:
+                    working_hours_weekend = "сб - вс выходной"
 
-            project_data_list.append({
-                'address': f'{one_office["address"]}',
-                'latlon': f'[{one_office["latitude"]}, {one_office["longitude"]}]',
-                'name': f'{one_office["name"]}',
-                'phones': f'[{one_office["phones"][0]["phone"]}]',
-                'working_hours': f"[пн - пт {working_hours['workdays']['startStr']} до {working_hours['workdays']['endStr']}, "
-                                 f"{working_hours_weekend}]",
-            }
-            )
+                project_data_list.append({
+                    'address': f'{one_office["address"]}',
+                    'latlon': f'[{one_office["latitude"]}, {one_office["longitude"]}]',
+                    'name': f'{one_office["name"]}',
+                    'phones': f'[{one_office["phones"][0]["phone"]}]',
+                    'working_hours': f"[пн - пт {working_hours['workdays']['startStr']} до {working_hours['workdays']['endStr']}, "
+                                     f"{working_hours_weekend}]",
+                }
+                )
 
-with open('tui_result.json', 'a', encoding='utf-8') as json_file:
-    json.dump(project_data_list, json_file, indent=4, ensure_ascii=False)
+    with open('tui_result.json', 'a', encoding='utf-8') as json_file:
+        json.dump(project_data_list, json_file, indent=4, ensure_ascii=False)
+    return print('Данные по городам сохранены в json-файл')
+
+
+if __name__ == '__main__':
+    parsing_api()
